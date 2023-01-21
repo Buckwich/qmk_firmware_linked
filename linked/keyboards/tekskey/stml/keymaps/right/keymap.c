500637a119ba94479ba6617c08489cec1538643c
Buckwich <Buckwich@users.noreply.github.com>
2023-01-21 23:02:16 +0100

    add rgb matrix

--- linked/keyboards/tekskey/stml/keymaps/right/keymap.c
+++ linked/keyboards/tekskey/stml/keymaps/right/keymap.c
@@ -5,7 +5,8 @@
 
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
-     _BASE
+    _BASE,
+    _RGB
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -17,6 +18,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_M  ,KC_N  ,KC_E   , KC_I, KC_O  , KC_QUOT,
         KC_K  ,KC_H  ,KC_COMM, KC_DOT, KC_SLSH  , KC_RSFT,
         KC_SPC,KC_ENT,KC_LBRC, KC_RBRC, KC_MINS  , KC_EQL
-       
+    ),
+    [_RGB] = LAYOUT(
+        RGB_TOG ,KC_F6 ,KC_F10 , KC_F11, KC_NO, QK_BOOT,
+        RGB_MOD ,RGB_HUI  ,RGB_SAI   , RGB_VAI, RGB_SPI  , KC_6,
+        RGB_RMOD,RGB_HUD  ,RGB_SAD   , RGB_VAD, RGB_SPD  , KC_BSLS,
+        KC_M  ,KC_N  ,KC_E   , KC_I, KC_O  , KC_QUOT,
+        KC_K  ,KC_H  ,KC_COMM, KC_DOT, KC_SLSH  , KC_RSFT,
+        KC_SPC,KC_ENT,KC_LBRC, KC_RBRC, KC_MINS  , KC_EQL
     ),
 }; 
