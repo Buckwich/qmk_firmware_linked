ffac965556fc75fdf8f89322dae7ab26056b0b97
Buckwich <Buckwich@users.noreply.github.com>
2023-10-15 21:21:34 +0200

    add layer rgb highlights

--- linked/keyboards/tekskey/rp/keymaps/via/keymap.c
+++ linked/keyboards/tekskey/rp/keymaps/via/keymap.c
@@ -22,3 +22,84 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS
         KC_LCTL,KC_LGUI,KC_LALT,RGB_TOG, KC_ENT,KC_SPC,     KC_SPC,KC_ENT,KC_LBRC,KC_RBRC,KC_MINS,KC_EQL
     )
 };
+
+#define AZURE   {132, 102, 255}
+#define BLACK   {  0,   0,   0}
+#define BLUE_   {170, 255, 255}
+#define CHART   { 64, 255, 255}
+#define CORAL   { 11, 176, 255}
+#define CYAN_   {128, 255, 255}
+#define GOLD_   { 36, 255, 255}
+#define GREEN   { 85, 255, 255}
+#define MAGEN   {213, 255, 255}
+#define ORNGE   { 21, 255, 255}
+#define PINK_   {234, 128, 255}
+#define PRPLE   {191, 255, 255}
+#define RED__   {  0, 255, 255}
+#define SPGRN   {106, 255, 255}
+#define TEAL_   {128, 255, 255}
+#define TURQU   {123,  90, 112}
+#define WHITE   {  0,   0, 255}
+#define YELLW   { 43, 255, 255}
+#define OFF__   {  0,   0,   0}
+#define _____   {  0,   0,   0}
+
+const uint8_t PROGMEM rgb_matrix_led_map[][RGB_MATRIX_LED_COUNT][4] = {
+   [1]= {
+    //LEFT
+    RED__,   _____,_____,   _____,_____,
+    _____,_____,MAGEN,TEAL_,PRPLE,_____, //REV
+    _____,_____,_____,_____,_____,_____,
+    _____,_____,_____,_____,_____,_____, //REV
+    _____,_____,_____,_____,_____,_____,
+    _____,_____,_____,_____,_____,_____, //REV
+
+    //RIGHT
+    _____,_____,   _____,_____,   _____,
+    MAGEN,MAGEN,MAGEN,RED__,_____,_____, //REV
+    _____,_____,TEAL_,TEAL_,TEAL_,MAGEN,
+    MAGEN,TEAL_,TEAL_,TEAL_,_____,_____, //REV
+    _____,PRPLE,TEAL_,TEAL_,TEAL_,MAGEN, 
+    MAGEN,TEAL_,TEAL_,PRPLE,PRPLE,PRPLE, //REV
+  }
+};
+
+void set_layer_color(int layer) {
+  for (int i = 0; i < RGB_MATRIX_LED_COUNT; i++) {
+    HSV hsv = {
+      .h = pgm_read_byte(&rgb_matrix_led_map[layer][i][0]),
+      .s = pgm_read_byte(&rgb_matrix_led_map[layer][i][1]),
+      .v = pgm_read_byte(&rgb_matrix_led_map[layer][i][2]),
+    };
+    if (!hsv.h && !hsv.s && !hsv.v) {
+        rgb_matrix_set_color( i, 0, 0, 0 );
+    } else {
+        RGB rgb = hsv_to_rgb( hsv );
+        float f = (float)rgb_matrix_config.hsv.v / UINT8_MAX;
+        rgb_matrix_set_color( i, f * rgb.r, f * rgb.g, f * rgb.b );
+    }
+  }
+}
+
+
+bool rgb_matrix_indicators_user(void) {
+  switch (biton32(layer_state)) {
+    // case 0:
+    //   set_layer_color(0);
+    //   break;
+    case 1:
+      set_layer_color(1);
+      break;	
+    // case 2:
+    //   set_layer_color(2);
+    //   break;
+    // case 3:
+    //   set_layer_color(3);
+    //   break;
+   default:
+    // if (rgb_matrix_get_flags() == LED_FLAG_NONE)
+    //   rgb_matrix_set_color_all(0, 0, 0);
+    break;
+  } 
+  return false;
+}
